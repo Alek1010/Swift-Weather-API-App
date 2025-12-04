5281610f6a361a9355f6b478a69d54bb35f9ef7a
Alek1010 <ali.mihaylov04@gmail.com>
2025-12-04 12:04:13 +0000

    just did a thing

--- WeatherDashboardTemplate/ViewModel/LocationManager.swift
+++ WeatherDashboardTemplate/ViewModel/LocationManager.swift
@@ -12,14 +12,34 @@ import CoreLocation
 
 @MainActor
 final class LocationManager {
+    
+    private let geocoder = CLGeocoder()
 
     func geocodeAddress(_ address: String) async throws -> (name: String, lat: Double, lon: Double) {
+        do {
+            let placemarks = try await geocoder.geocodeAddressString(address)
+            
+            guard let placemark = placemarks.first,
+                  let location = placemark.location else {
+                throw WeatherMapError.geocodingFailed("failed")
+            }
+            let name = placemark.locality ?? placemark.name ?? address
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            
+            return( name , lat, lon)
+            
+        } catch {
+            print("Geocoding failed:", error)
+            throw WeatherMapError.geocodingFailed("failed")
+        }
+    }
         // Uses `CLGeocoder` to convert a string address into geographic coordinates.
         // Extracts the name, latitude, and longitude from the first resulting placemark.
         // Throws a `WeatherMapError.geocodingFailed` if no valid location can be found.
 
         // DUMMY RETURN TO SATISFY COMPILER
-        preconditionFailure("Stubbed function not implemented. Requires a (name: String, lat: Double, lon: Double) return.")
+        
     }
 
     func findPOIs(lat: Double, lon: Double, limit: Int = 5) async throws -> [AnnotationModel] {
